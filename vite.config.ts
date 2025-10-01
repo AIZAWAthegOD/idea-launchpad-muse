f44a7f2b835049d675abb333b7affe4bd3c43740
AIZAWAthegOD <divineuchechukwu624@gmail.com>
2025-10-01 18:19:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/idea-launchpad-muse/',
+  base: '/rewards-hub-usa/',
   server: {
     host: "::",
     port: 8080,
